1d885f58e5da3808bd0b62eae6aed05862891c26
Edgar Carballo <karvayoedgar@gmail.com>
2017-03-02 22:45:01 -0600

    Agregar otro mapa

--- init.js
+++ init.js
@@ -5,6 +5,7 @@ window.addEventListener('load', () => {
 	new TipoDeHexagono('a', 0.25, '7F9ADF'); //agua
 	new TipoDeHexagono('p', 1, '9FAC1A'); //pasto
 	new TipoDeHexagono('m', 2, '8E97B6'); //montaña
+	new TipoDeHexagono('á', 2, '20FF00'); //boscoso
 	new Raza(
 		'Humano', 'A balanced race.', [
 			new TipoDeUnidad('Brave', 6, 3, 0, new Costo({
@@ -29,7 +30,19 @@ window.addEventListener('load', () => {
 		' Ā Ā Ā Ā Ā Ā ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ',
 		'Ā Ā Ā Ā ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ',
 		' Ā Ā ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ÿ ',
+	], new Coordenadas2D(-LADO_INICIAL_HEXAGONO, -LADO_INICIAL_HEXAGONO * Math.sqrt(3) / 2)),
+	new Mapa([
+		'Ā Ā Ā Ă Ă Ă Ā Ā Ā Ă Ā Ā Ā Ā Ā Ā Đ Đ Đ Đ',
+		' Ā Ā Ă Ă Ă Ā Ā Ā Ă Ă Ā Ā Ā Ā Đ Đ Đ Đ Đ ',
+		'Ā Ā Ā Ă Ă Ă Ă Ā Ă Ă Ē Đ Đ Đ Đ Đ Đ Đ Đ Đ',
+		' Ā Ā Ă Ă Ă ā ā ā Ă Ē Ē Đ Đ Đ Đ Đ Đ Đ Đ ',
+		'Ā Ā Ā Ā Ā ā ā Ā Ā Ă Ē Đ Đ Đ Đ Đ Đ Đ Đ Đ',
+		' Ā Ā Ā Ā Ā Ā Ā Ā Ă Ē Ē Ē Ē Ē Ē Ē Ē Đ Đ ',
+		'Ā Ā Ā Ā Ā Ā Ā Ā Ā Ă Ē Ē Ē Ē Ē Đ Đ Đ Đ Đ',
+		' Ā Ā Ā Ā Ā Ā Ā Ā Ē Ē Ē Ē Ē Ē Ē Ē Đ Đ Đ ',
+		'Ā Ā Ā Ā Ā Ā Ā Ā Đ Ē Đ Ē Ē Ē Ē Đ Đ Đ Đ Đ',
+		' Ā Ā Ā Ā Ā Ā Đ Đ Đ Đ Đ Ē Ē Ē Đ Đ Đ Đ Đ ',
 	], new Coordenadas2D(-LADO_INICIAL_HEXAGONO, -LADO_INICIAL_HEXAGONO * Math.sqrt(3) / 2));
 
-	document.getElementById('container').style.display = 'block';
+	document.getElementById('container').style.disĐlay = 'block';
 });
